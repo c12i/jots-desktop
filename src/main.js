c654e96449d00066cff34c3771a71b3ac2b8f865
Collins Muriuki <murerwacollins@gmail.com>
2021-02-21 01:33:38 +0300

    Implement existing web app to electron and set up csp

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron')
-const { is } = require('electron-util')
+const { is, setContentSecurityPolicy } = require('electron-util')
+const config = require('./config')
 
 let window
 
@@ -8,14 +9,33 @@ function createWindow() {
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: false
         }
     })
 
-    window.loadFile('index.html')
+    if (is.development) {
+        window.loadURL(config.LOCAL_WEB_URL)
+    } else {
+        window.loadURL(config.PRODUCTION_WEB_URL)
+    }
 
     if (is.development) {
         window.webContents.openDevTools()
+    }
+
+    // set the CSP in production mode
+    if (!is.development) {
+        setContentSecurityPolicy(`
+            default-src 'none';
+            script-src 'self';
+            img-src 'self' https://www.gravatar.com;
+            style-src 'self' 'unsafe-inline';
+            font-src 'self';
+            connect-src 'self' ${config.PRODUCTION_API_URL};
+            base-uri 'none';
+            form-action 'none';
+            frame-ancestors 'none';
+        `)
     }
 
     window.on('closed', () => {
